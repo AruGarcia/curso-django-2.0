550c61b108eb4767e3463fc2245cc38132e10ead
Aruana <aruanagarcia@gmail.com>
2023-07-29 12:36:40 -0300

    Instalação e configuração do Collectfest3.

--- pypro/settings.py
+++ pypro/settings.py
@@ -33,7 +33,8 @@ ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 
 # Application definition
-
+STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+COLLECTFAST_STRATEGY = "collectfast.strategies.boto3.Boto3Strategy"
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
