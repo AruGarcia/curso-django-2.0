eb245bb362ba0518518324a5ed80ae6a63999698
Aruana <aruanagarcia@gmail.com>
2023-07-29 12:54:40 -0300

    Comentário e organização do Collectfest3.

--- pypro/settings.py
+++ pypro/settings.py
@@ -33,8 +33,11 @@ ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 
 # Application definition
+
+# configurações do collectfast
 STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 COLLECTFAST_STRATEGY = "collectfast.strategies.boto3.Boto3Strategy"
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
