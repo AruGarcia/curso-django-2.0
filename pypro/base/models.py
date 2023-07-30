f04350f7a7bc2fcb33abfda2afce267d26fa7bbd
Aruana <aruanagarcia@gmail.com>
2023-07-30 01:07:24 -0300

    substituição pelo modelo do renzo.

--- pypro/base/models.py
+++ pypro/base/models.py
@@ -5,76 +5,71 @@ from django.db import models
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
-
 
 class UserManager(BaseUserManager):
     use_in_migrations = True
 
     def _create_user(self, email, password, **extra_fields):
         """
-        Create and save a user with the given username, email, and password.
+        Creates and saves a User with the given email and password.
         """
         if not email:
-            raise ValueError("The given username must be set")
+            raise ValueError('The given email must be set')
         email = self.normalize_email(email)
-        # Lookup the real model class from the global app registry so this
-        # manager method can be used in migrations. This is fine because
-        # managers are by definition working on the real model.
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
     def create_user(self, email, password=None, **extra_fields):
-        extra_fields.setdefault("is_superuser", False)
+        extra_fields.setdefault('is_superuser', False)
         return self._create_user(email, password, **extra_fields)
 
     def create_superuser(self, email, password, **extra_fields):
-        extra_fields.setdefault("is_staff", True)
-        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_staff', True)
 
-        if extra_fields.get("is_staff") is not True:
-            raise ValueError("Superuser must have is_staff=True.")
-        if extra_fields.get("is_superuser") is not True:
-            raise ValueError("Superuser must have is_superuser=True.")
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
 
         return self._create_user(email, password, **extra_fields)
 
 
 class User(AbstractBaseUser, PermissionsMixin):
     """
-    App base user class.
+    App base User class.
 
     Email and password are required. Other fields are optional.
     """
 
-    first_name = models.CharField(_("first name"), max_length=150, blank=True)
-    email = models.EmailField(_("email address"), unique=True)
+    first_name = models.CharField(_('first name'), max_length=30, blank=True)
+    email = models.EmailField(_('email address'), unique=True)
     is_staff = models.BooleanField(
-        _("staff status"),
+        _('staff status'),
         default=False,
-        help_text=_("Designates whether the user can log into this admin site."),
+        help_text=_('Designates whether the user can log into this admin site.'),
     )
     is_active = models.BooleanField(
-        _("active"),
+        _('active'),
         default=True,
         help_text=_(
-            "Designates whether this user should be treated as active. "
-            "Unselect this instead of deleting accounts."
+            'Designates whether this user should be treated as active. '
+            'Unselect this instead of deleting accounts.'
         ),
     )
-    date_joined = models.DateTimeField(_("date joined"), default=timezone.now)
+    date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
 
     objects = UserManager()
 
-    EMAIL_FIELD = "email"
-    USERNAME_FIELD = "email"
+    EMAIL_FIELD = 'email'
+    USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
     class Meta:
-        verbose_name = _("user")
-        verbose_name_plural = _("users")
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
 
     def clean(self):
         super().clean()
@@ -84,7 +79,7 @@ class User(AbstractBaseUser, PermissionsMixin):
         """
         Return the first_name plus the last_name, with a space in between.
         """
-        full_name = "%s" % (self.first_name)
+        full_name = '%s' % (self.first_name)
         return full_name.strip()
 
     def get_short_name(self):
@@ -93,5 +88,4 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def email_user(self, subject, message, from_email=None, **kwargs):
         """Send an email to this user."""
-
         send_mail(subject, message, from_email, [self.email], **kwargs)
