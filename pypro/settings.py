757a39b0aed9151586541375579c4d7238f6ed2a
Aruana <aruanagarcia@gmail.com>
2023-07-31 14:56:34 -0300

    Instalação do django_debug_toolbar, configuração da lib, edição settings para funcionar2.

--- pypro/settings.py
+++ pypro/settings.py
@@ -146,9 +146,9 @@ AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
 # STORAGE CONFIGURATION IN S3 AWS
 # ___________________________________________________
 if AWS_ACCESS_KEY_ID:
-    COLLECTFAST_ENABLED = True
-    STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-    COLLECTFAST_STRATEGY = "collectfast.strategies.boto3.Boto3Strategy"
+    # COLLECTFAST_ENABLED = True
+    # STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+    # COLLECTFAST_STRATEGY = "collectfast.strategies.boto3.Boto3Strategy"
     AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400', }
