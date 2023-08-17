6eef9ed5dde03506c13bbe96ec6784a30ed34174
Aruana <aruanagarcia@gmail.com>
2023-08-16 23:27:05 -0300

    Edição do slug.

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 
 
 def video(request, slug):
-    video = {'titulo': 'Video Aperitivo: Motivação', 'vimeo_id': 853846914}
+    video = {
+    'motivacao': {'titulo': 'Video Aperitivo: Motivação', 'vimeo_id': 853846914},
+    'instalacao-windows': {'titulo': 'Intalação Windows', 'vimeo_id: '}
+    }
+    video = video[slug]
     return render(request, 'aperitivos/video.html', context={'video': video})
