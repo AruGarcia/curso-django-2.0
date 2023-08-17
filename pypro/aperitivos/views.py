aea3f361de12a484d36f0f16907de89b487c62ae
Aruana <aruanagarcia@gmail.com>
2023-08-16 23:38:09 -0300

    adição de pagina instalação do windows.

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render
 
 def video(request, slug):
     video = {
-    'motivacao': {'titulo': 'Video Aperitivo: Motivação', 'vimeo_id': 853846914},
-    'instalacao-windows': {'titulo': 'Intalação Windows', 'vimeo_id: '}
+        'motivacao': {'titulo': 'Video Aperitivo: Motivação', 'vimeo_id': 853846914},
+        'instalacao-windows': {'titulo': 'Intalação Windows', 'vimeo_id': 853846914},
     }
     video = video[slug]
     return render(request, 'aperitivos/video.html', context={'video': video})
