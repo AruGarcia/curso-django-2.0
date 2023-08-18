0258fd2ab650c6c630e121c24bc5cae4cd90c9bd
Aruana <aruanagarcia@gmail.com>
2023-08-18 18:11:03 -0300

    Class video adicionada para os videos.

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
 
+
+class Video:
+    def __init__(self, slug, titulo, vimeo_id):
+        self.slug = slug
+        self.titulo = titulo
+        self.vimeo_id = vimeo_id
+
+
 videos = [
-    {'slug': 'motivacao', 'titulo': 'Video Aperitivo: Motivação', 'vimeo_id': 853846914},
-    {'slug': 'instalacao-windows', 'titulo': 'Instalação Windows', 'vimeo_id': 853846914},
+    Video('motivacao', 'Video Aperitivo: Motivação', 853846914),
+    Video('instalacao-windows', 'Instalação Windows', 853846914),
 ]
 
-videos_dct = {dct['slug']: dct for dct in videos}
+videos_dct = {v.slug: v for v in videos}
 
 
 def indice(request):
