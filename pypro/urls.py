8ae1c8b154cff74f5fca4bd9d98ec31bdbdf3942
Aruanã Garcia <aruanagarcia@gmail.com>
2023-10-09 16:40:03 -0300

    Cridado os testes e templte para a pagina de login.

--- pypro/urls.py
+++ pypro/urls.py
@@ -21,10 +21,12 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('contas/', include('django.contrib.auth.urls')),
     path('', include('pypro.base.urls')),
     path('aperitivos/', include('pypro.aperitivos.urls')),
     path('modulos/', include('pypro.modulos.urls')),
     path('turmas/', include('pypro.turmas.urls')),
+
 ]
 
 if settings.DEBUG:
